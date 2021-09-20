4616685506b7ab018ec3fd3c5d2308fba0d3e7c4
Adam Sasine <adam.sasine@gmail.com>
2021-09-20 19:46:55 -0400

    Refactored request handlers using decorators

--- DiscordWebhook/__init__.py
+++ DiscordWebhook/__init__.py
@@ -1,75 +1,17 @@
 import logging
-from typing import Iterable, Mapping, Optional
 
 import azure.functions as func
-from discord_interactions import (InteractionResponseType, InteractionType,
-                                  verify_key)
 
-import handlers
-from models import CommandOptionType
+from DiscordWebhook.handlers import (BadRequest, application_command_handler,
+                                     partial_http_request_handler,
+                                     ping_handler,
+                                     verify_request_signature_handler)
 
 CLIENT_PUBLIC_KEY = '2ec6232217f7b9c05cf478d51678a5521e6696276308a23c26df928a23c3c505'
 
-subcommand_to_handler = {
-    'status': handlers.status,
-    'start': handlers.start,
-    'stop': handlers.stop,
-}
-
-def handle_application_command(json: Mapping) -> func.HttpResponse:
-    command_name = json.get('data').get('name')
-    logging.info('Processing application command: %s', command_name)
-    if command_name == 'factorio':
-        options: Optional[Iterable[Mapping]] = json.get('data').get('options')
-        logging.debug('There are %d option(s)', len(options) if options is not None else 0)
-        if options is not None and len(options) == 1:
-            option = options[0]
-            option_type = option.get('type')
-            logging.debug('Option type: %d', option_type)
-            if option_type == CommandOptionType.SUB_COMMAND:
-                # option is a subcommand
-                subcommand_name = option.get('name')
-                logging.debug('Subcommand name: %s', subcommand_name)
-                handler = subcommand_to_handler.get(subcommand_name)
-                if handler is not None:
-                    return handler()
-
-
-    logging.warn('Unknown command: %s', command_name)
-    return handlers.BadRequest('Unknown command name')
-
-
+@partial_http_request_handler(verify_request_signature_handler(CLIENT_PUBLIC_KEY))
+@partial_http_request_handler(ping_handler)
+@partial_http_request_handler(application_command_handler)
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    # TODO(asasine): make a function wrapper for key verification
-    # TODO(asasine): make a function wrapper for ping requests
-    signature = req.headers.get('X-Signature-Ed25519')
-    timestamp = req.headers.get('X-Signature-Timestamp')
-    if signature is None or timestamp is None:
-        return handlers.BadRequest('Bad request signature')
-        
-    try:
-        body: bytes = req.get_body()
-    except ValueError:
-        return handlers.BadRequest('Bad request signature')
-
-    if not verify_key(body, signature, timestamp, CLIENT_PUBLIC_KEY):
-        return handlers.BadRequest('Bad request signature')
-
-    # if it's a PING request, respond immediately
-    try:
-        body_json: Mapping = req.get_json()
-    except ValueError:
-        return handlers.BadRequest('Bad request signature')
-
-    request_type = body_json.get('type')
-    if request_type == InteractionType.PING:
-        logging.info('Got a PING')
-        return handlers.Ok({
-            'type': InteractionResponseType.PONG
-        })
-    elif request_type == InteractionType.APPLICATION_COMMAND:
-        return handle_application_command(body_json)
-    else:
-        return handlers.BadRequest('Unknown request type: %d', request_type)
+    logging.warn('Function was not handled by middleware.')
+    return BadRequest('Bad request')
--- DiscordWebhook/handlers.py
+++ DiscordWebhook/handlers.py
@@ -1,9 +1,13 @@
-
+import functools
 import json
-from typing import Mapping, Union
+import logging
+from typing import Callable, Iterable, Mapping, Optional, Union
 
 import azure.functions as func
-from discord_interactions import InteractionResponseType
+from discord_interactions import (InteractionResponseType, InteractionType,
+                                  verify_key)
+
+from DiscordWebhook.models import CommandOptionType
 
 
 def Ok(body: Union[str, Mapping]) -> func.HttpResponse:
@@ -15,7 +19,79 @@ def Ok(body: Union[str, Mapping]) -> func.HttpResponse:
 
     
 def BadRequest(body: str) -> func.HttpResponse:
+    return func.HttpResponse(body=body, status_code=400)
+
+
+def Unauthorized(body: str) -> func.HttpResponse:
     return func.HttpResponse(body=body, status_code=401)
+    
+
+def partial_http_request_handler(handler: Callable[[func.HttpRequest], Optional[func.HttpResponse]]) -> Callable[[func.HttpRequest], func.HttpResponse]:
+    """
+    Creates a decorator from an HttpRequest handler.
+    
+    The handler returns None to indicate the request should be forwarded to the next handler.
+    
+    Args:
+        handler:
+            A callable which accepts an HttpRequest and returns an HttpResponse or None.
+            If the returned value is None, the request is forwarded to the next handler.
+            Otherwise, the pipeline is terminated and the returned value is the final return value.
+
+    Returns:
+        A decorator.
+        The decorator wraps a function which accepts an HttpRequest and returns an HttpResponse
+    """
+    def decorator(f: Callable[[func.HttpRequest], func.HttpResponse]):
+        @functools.wraps(f)
+        def inner_decorator(req: func.HttpRequest) -> func.HttpResponse:
+            response = handler(req)
+            if response is not None:
+                # handler handled the request
+                return response
+            else:
+                # handler did not handle the request
+                return f(req)
+        return inner_decorator
+    return decorator
+
+
+def verify_request_signature_handler(client_public_key: str) -> Callable[[func.HttpRequest], Optional[func.HttpResponse]]:
+    def inner(req: func.HttpRequest) -> Optional[func.HttpResponse]:
+        logging.debug('Verifying request signature')
+        signature = req.headers.get('X-Signature-Ed25519')
+        timestamp = req.headers.get('X-Signature-Timestamp')
+        if signature is None or timestamp is None:
+            return Unauthorized('Bad request signature')
+            
+        try:
+            body: bytes = req.get_body()
+        except ValueError:
+            return Unauthorized('Bad request signature')
+
+        if not verify_key(body, signature, timestamp, client_public_key):
+            return Unauthorized('Bad request signature')
+
+        return None
+
+    return inner
+
+
+def ping_handler(req: func.HttpRequest) -> Optional[func.HttpResponse]:
+    try:
+        body_json: Mapping = req.get_json()
+    except ValueError:
+        # unable to handle this request without JSON data
+        return None
+
+    request_type = body_json.get('type')
+    if request_type == InteractionType.PING:
+        logging.info('Got a PING')
+        return Ok({
+            'type': InteractionResponseType.PONG
+        })
+
+    return None
 
 
 def status() -> func.HttpResponse:
@@ -26,6 +102,7 @@ def status() -> func.HttpResponse:
         }
     })
 
+
 def start() -> func.HttpResponse:
     return Ok({
         'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -34,6 +111,7 @@ def start() -> func.HttpResponse:
         }
     })
 
+
 def stop() -> func.HttpResponse:
     return Ok({
         'type': InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
@@ -41,3 +119,46 @@ def stop() -> func.HttpResponse:
             'content': 'stop'
         }
     })
+
+
+subcommand_to_handler = {
+    'status': status,
+    'start': start,
+    'stop': stop,
+}
+
+
+def application_command_handler(req: func.HttpRequest) -> Optional[func.HttpResponse]:
+    try:
+        json: Mapping = req.get_json()
+    except ValueError:
+        # unable to handle this request without JSON data
+        logging.debug('Unable to get JSON data')
+        return None
+
+    
+    request_type = json.get('type')
+    if request_type != InteractionType.APPLICATION_COMMAND:
+        logging.debug(f'Request type {request_type} is not {InteractionType.APPLICATION_COMMAND}')
+        return None
+
+    command_name = json.get('data').get('name')
+    logging.info('Processing application command: %s', command_name)
+    if command_name == 'factorio':
+        options: Optional[Iterable[Mapping]] = json.get('data').get('options')
+        logging.debug('There are %d option(s)', len(options) if options is not None else 0)
+        if options is not None and len(options) == 1:
+            option = options[0]
+            option_type = option.get('type')
+            logging.debug('Option type: %d', option_type)
+            if option_type == CommandOptionType.SUB_COMMAND:
+                # option is a subcommand
+                subcommand_name = option.get('name')
+                logging.debug('Subcommand name: %s', subcommand_name)
+                handler = subcommand_to_handler.get(subcommand_name)
+                if handler is not None:
+                    return handler()
+
+
+    logging.warn('Unknown command: %s', command_name)
+    return None
